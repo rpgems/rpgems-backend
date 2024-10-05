874eb6ddd37653dac461736373c04f294c37a6ed
Marco Gomes <malgomes@gmail.com>
2024-10-05 03:02:33 -0300

    feat: add class and attribute routes to FastAPI

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from app.core.settings import get_settings
 
 from app.api import router as main_router
 from app.api.routes.character import router as character_router
+from app.api.routes.character_class import router as character_class_router
+from app.api.routes.character_attribute import router as character_attribute_router
 
 
 def create_app() -> FastAPI:
@@ -18,6 +20,8 @@ def create_app() -> FastAPI:
 
     _app.include_router(main_router, prefix=settings.API_V1_STR)
     _app.include_router(character_router, prefix=settings.API_V1_STR)
+    _app.include_router(character_class_router, prefix=settings.API_V1_STR)
+    _app.include_router(character_attribute_router, prefix=settings.API_V1_STR)
 
     return _app
 
