649567bc3c174ed367436cf8c6e1688cfed132a5
Marco Gomes <malgomes@gmail.com>
2024-10-04 23:22:27 -0300

    feature: add GET,PUT,POST and DELETE endpoints for character in api

--- app/character/app_router.py
+++ app/character/app_router.py
@@ -1,9 +1,13 @@
+from typing import List
+
 from fastapi import APIRouter, status, HTTPException
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
 from app.character.exceptions import CharacterNotFound
 from app.character.model import Character
-from app.character.db import get_character_by_id
+from app.character.db import get_character_by_id, list_all_characters, search_characters_by_name, create_character, \
+    delete_character_by_id, update_character_definition
 
 router = APIRouter(prefix="/character", tags=["character"])
 
@@ -29,4 +33,108 @@ async def get_character(character_id: int) -> CharacterResponse:
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
     response = CharacterResponse(character_definition=character_response)
+    return response
+
+
+@router.get(
+    path="/",
+    responses={
+        status.HTTP_200_OK: {"description": "Characters found, returning list of characters"},
+        status.HTTP_400_BAD_REQUEST: {"description": "Invalid request"},
+        status.HTTP_404_NOT_FOUND: {"description": "No character found"}
+    },
+    response_model=List[CharacterResponse],
+)
+async def list_characters() -> List[CharacterResponse]:
+    try:
+        characters = list_all_characters()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    if len(characters) == 0:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No characters found")
+    response = [CharacterResponse(character_definition=character) for character in characters]
+    return response
+
+
+@router.get(
+    path="/search",
+    responses={
+        status.HTTP_200_OK: {"description": "Characters found, returning list of characters"},
+        status.HTTP_400_BAD_REQUEST: {"description": "Invalid request"},
+        status.HTTP_404_NOT_FOUND: {"description": "No character found"}
+    },
+    response_model=List[CharacterResponse],
+)
+async def list_characters_by_name(name_search: str) -> List[CharacterResponse]:
+    try:
+        characters = search_characters_by_name(name_search)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    if len(characters) == 0:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No characters found")
+    response = [CharacterResponse(character_definition=character) for character in characters]
+    return response
+
+
+@router.post(
+    path="/",
+    responses={
+        status.HTTP_201_CREATED: {"description": "Character created"},
+        status.HTTP_400_BAD_REQUEST: {"description": "Invalid request"},
+    },
+    response_model=JSONResponse,
+)
+async def create_new_character(character_definition: Character) -> JSONResponse:
+    try:
+        character_id = create_character(character_definition)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    content = {"message": "Character created"}
+    headers = {"Content-Type": "application/json", "Location": f"/character/{character_id}"}
+    return JSONResponse(content=content, status_code=status.HTTP_201_CREATED, headers=headers)
+
+
+@router.delete(
+    path="/{character_id}",
+    responses={
+        status.HTTP_204_NO_CONTENT: {"description": "Character deleted"},
+        status.HTTP_400_BAD_REQUEST: {"description": "Invalid request"},
+        status.HTTP_404_NOT_FOUND: {"description": "Character not found"}
+    },
+    response_model=JSONResponse,
+)
+async def delete_character(character_id: int) -> JSONResponse:
+    try:
+        delete_character_by_id(character_id)
+    except CharacterNotFound as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    content = {"message": "Character deleted"}
+    headers = {"Content-Type": "application/json"}
+    return JSONResponse(content=content, status_code=status.HTTP_204_NO_CONTENT, headers=headers)
+
+
+@router.put(
+    path="/{character_id}",
+    responses={
+        status.HTTP_200_OK: {"description": "Character updated"},
+        status.HTTP_201_CREATED: {"description": "Character created"},
+        status.HTTP_400_BAD_REQUEST: {"description": "Invalid request"},
+    },
+    response_model=JSONResponse,
+)
+async def update_character(character_id: int, character_definition: Character) -> JSONResponse:
+    try:
+        update_character_definition(character_id, character_definition)
+        content = {"message": "Character updated"}
+        headers = {"Content-Type": "application/json"}
+        response = JSONResponse(content=content, status_code=status.HTTP_200_OK, headers=headers)
+    except CharacterNotFound as e:
+        character_id = create_character(character_definition)
+        content = {"message": "Character updated"}
+        headers = {"Content-Type": "application/json", "Location": f"/character/{character_id}"}
+        response = JSONResponse(content=content, status_code=status.HTTP_201_CREATED, headers=headers)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
     return response
--- app/character/db.py
+++ app/character/db.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from app.character.exceptions import CharacterNotFound, BubblesException
 from app.character.model import Character, CharacterClass, Attribute
 
@@ -43,3 +45,30 @@ def get_character_by_id(character_id: int) -> Character:
         extended_attributes = get_character_extended_attributes(character.id)
         character.character_attributes = extended_attributes
     return character
+
+
+def list_all_characters() -> List[Character]:
+    # TODO should add the logic to actually list all character in the DB
+    character = get_character_by_id(1)
+    return [character]
+
+
+def search_characters_by_name(name_search: str) -> List[Character]:
+    # TODO should add the logic to actually search characters by name
+    character = get_character_by_id(1)
+    return [character]
+
+
+def create_character(character: Character) -> int:
+    # TODO should add the logic to actually add character to the DB
+    return 2
+
+
+def delete_character_by_id(character_id: int) -> None:
+    # TODO should add the logic to actually delete character in the DB
+    character = get_character_by_id(character_id)
+
+
+def update_character_definition(character_id: int, character_definition: Character) -> None:
+    # TODO should add the logic to actually update character in the DB
+    get_character_by_id(character_id)
