fbba628787139caaff61627a89945becd1f9242d
Marco Gomes <malgomes@gmail.com>
2024-10-24 23:43:44 -0300

    refactoor: fix duplicate code found by pylint

--- app/domain/character.py
+++ app/domain/character.py
@@ -2,12 +2,14 @@
 from typing import List, Optional
 from pydantic import BaseModel
 
+from app.domain.character_class import CharacterClass
+
 
 class Character(BaseModel):
     """Character model"""
     id: int
     name: str
-    character_class: int
+    character_class: int | CharacterClass
     description: str
     experience_points: int
     character_attributes: Optional[List[int]] = None
